b00af47652cb7d33354953d9f3d5c9d4f737220b
jian <xiangjian1990s@gmail.com>
2020-03-17 10:17:24 +0800

    a

---
+++ src/main/java/com/hzdl/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.hzdl.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
